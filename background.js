58f506473dd47ba14b23dc70067a6fadb05a641a
Swayam Chidrawar <schidraw@calpoly.edu>
2023-07-02 23:11:10 -0700

    Extend functionality to modify dates on modules  page

--- background.js
+++ background.js
@@ -10,20 +10,37 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 function runScript() {
     console.log("Script is Running");
     setInterval(() => {
-        let dueDates = document.querySelectorAll('span[data-html-tooltip-title]');
+        let dueDates = document.querySelectorAll('span[data-html-tooltip-title], .due_date_display');
 
         dueDates.forEach(dueDate => {
-            let dueDateText = dueDate.getAttribute('data-html-tooltip-title');
-            let dateParts = dueDateText.match(/(\w+)\s(\d+)\s\w+\s(\d+):(\d+)(\w+)/);
+            let dueDateText;
+            let dateParts;
+            if (dueDate.matches('span[data-html-tooltip-title]')) {
+                dueDateText = dueDate.getAttribute('data-html-tooltip-title');
+                dateParts = dueDateText.match(/(\w+)\s(\d+)\s\w+\s(\d+):(\d+)(\w+)/);
+            } else {
+                dueDateText = dueDate.textContent;
+                dateParts = dueDateText.match(/(\w+)\s(\d+)/);
+            }
             let monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             let monthIndex = monthNames.indexOf(dateParts[1]);
             let year = new Date().getFullYear();
-            let dueDateTime = new Date(year, monthIndex, dateParts[2], dateParts[5] === 'pm' ? parseInt(dateParts[3]) + 12 : dateParts[3], dateParts[4]);
+            let dueDateTime;
+            if (dueDate.matches('span[data-html-tooltip-title]')) {
+                dueDateTime = new Date(year, monthIndex, dateParts[2], dateParts[5] === 'pm' ? parseInt(dateParts[3]) + 12 : dateParts[3], dateParts[4]);
+            } else {
+                dueDateTime = new Date(year, monthIndex, dateParts[2]);
+            }
             let daysLeft = Math.ceil((dueDateTime - new Date()) / (1000 * 60 * 60 * 24)) - 1;
             let daysLeftText = daysLeft > 0 ? `${daysLeft} days left` : daysLeft == 0 ? 'Due today' : 'Due date passed';
             let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
             let dayOfWeekText = daysOfWeek[dueDateTime.getUTCDay()];
-            dueDate.textContent = `${dueDateText} (${dayOfWeekText}, ${daysLeftText})`;
+
+            if (!dueDate.getAttribute('data-modified')) {
+                dueDate.textContent = `${dueDateText} (${dayOfWeekText}, ${daysLeftText})`;
+                dueDate.setAttribute('data-modified', true);
+            }
         });
     }, 100);
 }
+
